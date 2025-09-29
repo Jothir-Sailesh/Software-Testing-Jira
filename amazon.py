e06c651038329a071c175f4ad9315a5cfcb0687b
Jothir Sailesh <Jothirsailesh005@email.com>
2025-09-29 09:45:02 +0530

    Amazon project script added

---
+++ amazon.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+from twisted.mail.alias import handle
+
+driver = webdriver.Chrome()
+driver.get("https://www.amazon.in/")
+searchBar = driver.find_element(By.ID, "twotabsearchtextbox")
+searchBar.click()
+searchBar.send_keys("Sony PS5")
+searchButton = driver.find_element(By.ID, "nav-search-submit-button")
+searchButton.click()
+product = driver.find_element(By.LINK_TEXT, "Sony PlayStation5 Gaming Console (Slim)")
+product.click()
+time.sleep(5)
+
+driver.switch_to.window(driver.window_handles[-1])
+
+addToCart = driver.find_element(By.ID, "add-to-cart-button")
+addToCart.click()
+time.sleep(10)
+
+goToCart = driver.find_element(By.LINK_TEXT, "Go to Cart")
+goToCart.click()
+
+proceedToBuy = driver.find_element(By.NAME, "proceedToRetailCheckout")
+proceedToBuy.click()
+
+
+time.sleep(8)
+email = driver.find_element(By.ID, "ap_email_login")
+email.send_keys("jothirsailesh005@gmail.com")
+
+continue_button = driver.find_element(By.ID, "continue-announce")
+continue_button.click()
+
+
+time.sleep(8)
+password = driver.find_element(By.ID, "ap_password")
+password.send_keys("#Jothir55Sailesh55")
+
+sign_in_button = driver.find_element(By.LINK_TEXT, "Sign in")
+sign_in_button.click()
+
+time.sleep(100)
+
+
